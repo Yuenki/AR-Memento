64b190064a1a906459e1eb34821804b39c7311b9
Fernando Herrera <fherrera2018@gmail.com>
2019-11-09 18:26:22 -0800

    Implemented a second screen. Added a button that is able to switch to a 'camera' screen. I will work on implementing AR stuff in this second screen titled CameraActivity.

--- app/src/main/java/com/example/ar_memento/MainActivity.java
+++ app/src/main/java/com/example/ar_memento/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.ar_memento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn_go_to_camera);
+        onResume();
+    }
+    protected void onResume() {
+        super.onResume();
+        final Intent cameraIntent = new Intent(this, CameraActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(cameraIntent);
+            }
+        });
     }
 }
