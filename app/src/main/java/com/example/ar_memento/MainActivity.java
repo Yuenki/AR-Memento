f5f1cb34fec50e4fc79b3396dc95598a12bfd3de
Ky (kee) <kyuenki@gmail.com>
2019-11-13 18:10:12 -0800

    added home screen logo and changed app button

--- app/src/main/java/com/example/ar_memento/MainActivity.java
+++ app/src/main/java/com/example/ar_memento/MainActivity.java
@@ -6,22 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    ImageButton slide_up_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.btn_go_to_camera);
+
+        slide_up_button = (ImageButton) findViewById(R.id.imageButton);
         onResume();
     }
     protected void onResume() {
         super.onResume();
         final Intent cameraIntent = new Intent(this, CameraActivity.class);
-        button.setOnClickListener(new View.OnClickListener() {
+        slide_up_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(cameraIntent);
             }
