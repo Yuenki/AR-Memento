94db63698ed87e9e8517438cf090d7ae53145970
Gerardo89e <mgerardo761@gmail.com>
2020-01-14 12:26:09 -0800

    added hamburger feature to the app with navigation bar

--- app/src/main/java/com/example/ar_memento/MainActivity.java
+++ app/src/main/java/com/example/ar_memento/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ar_memento;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,13 +20,17 @@ public class MainActivity extends AppCompatActivity {
     ImageButton slide_up_button;
     Button ImgButton;
 
-
-
+    private DrawerLayout mNavDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.nav_drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mNavDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mNavDrawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        mNavDrawer.addDrawerListener(toggle);
+        toggle.syncState();
         slide_up_button = (ImageButton) findViewById(R.id.imageButton);
         ImgButton  =findViewById(R.id.ImgButton);
         onResume();
@@ -30,10 +38,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onBackPressed() {
+        if(mNavDrawer.isDrawerOpen(GravityCompat.START)){
+            mNavDrawer.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+
+        }
     }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.main, menu);
+            return super.onCreateOptionsMenu(menu);
+        }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
