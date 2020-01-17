6889a61c9afd6a7729dc9823689f35f768d4427a
Fernando Herrera <fherrera2018@gmail.com>
2020-01-17 10:46:44 -0800

    Setting up the files necessary to implement scanning. The xml and android manifest have been set up. Completing ScannerActivity.java and ScannerARFragment.java should give us what we want!

---
+++ app/src/main/java/com/example/ar_memento/ScannerARFragment.java
@@ -0,0 +1,8 @@
+package com.example.ar_memento;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class ScannerARFragment extends ArFragment {
+    // This is not an activity because we are not going to set view here!
+    // This is to facilitate staging the scanner.
+}
---
+++ app/src/main/java/com/example/ar_memento/ScannerActivity.java
@@ -0,0 +1,6 @@
+package com.example.ar_memento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScannerActivity extends AppCompatActivity {
+}
