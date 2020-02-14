8db3ea9062aab27f6cd2bf8a4f55cb53d7a76e59
Ky (kee) <kyuenki@gmail.com>
2020-02-14 12:24:32 -0800

    loging java

--- app/src/main/java/com/example/ar_memento/login.java
+++ app/src/main/java/com/example/ar_memento/login.java
@@ -1,7 +1,19 @@
 package com.example.ar_memento;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class login extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
+public class Login extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.login,container,false);
+    }
 }
